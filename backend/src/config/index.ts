79004f4a7bb11e38e04d5b2d17374b2bb4fd75be
KelvinKoech <koechkelvin2001@gmail.com>
2023-02-10 21:29:49 +0300

    configurations

---
+++ backend/src/config/index.ts
@@ -0,0 +1,16 @@
+const sql = require('mssql')
+const sqlConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  server: 'localhost',
+  pool: {
+    max: 100,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: false, 
+    trustServerCertificate: false 
+  }
+}
