1598d6987c18fd6bfaeb03eebb152409c29558a2
Milgo21 <felixmilgo21@gmail.com>
2023-02-13 22:51:35 +0300

    products front end

---
+++ frontend/src/admin/users.ts
@@ -0,0 +1,50 @@
+const baseURL = 'http://localhost:4000';
+
+const request = {
+    getData: async(route: string) => {
+        let res = await fetch(`${baseURL}${route}`);
+        let data = await res.json();
+        return data;
+    },
+    postData: async(route: string, body: any) => {
+       const res = await fetch(baseURL+route,  {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }});
+            return res;
+    },
+    deleteData: () => {
+
+    },
+    updateData: () => {
+
+    }
+}
+
+// Function to get and populate products
+const getProducts = async() => {
+try {
+    const products = await request.getData('/products/5');
+    console.log(products, 'pro');
+    
+} catch (error: any) {
+    console.log(error.message);
+    
+}
+
+}
+// Run this on page load
+getProducts();
+// Test post
+const data =   {
+                    Product_name: "Khaki trousers",
+                    Product_price: "1900.00",
+                    Category_name: "Clothes"};
+const postProduct = async() => {
+    const res = await request.postData('/products', data);
+    console.log(res);
+    
+}                    
+// postProduct()
--- frontend/src/carousel.ts
+++ frontend/src/carousel.ts
@@ -6,14 +6,14 @@ function showSlides() {
   let slides = document.getElementsByClassName("banner__item");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < 3; i++) {
-    slides[i].style.display = "none";  
+    // slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > 3) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" activedot", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  // slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " activedot";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
