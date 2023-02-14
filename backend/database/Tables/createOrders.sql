e10996081928be580ffa69d9e3077accfe5ed6e3
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-02-14 15:56:23 +0300

    updated code

--- backend/database/Tables/createOrders.sql
+++ backend/database/Tables/createOrders.sql
@@ -4,6 +4,38 @@ UserId VARCHAR(100) ,
  productId VARCHAR(100),
  orderStatus VARCHAR(100) CHECK (orderStatus IN ('pending','fulfilled','cancelled')) DEFAULT 'pending',
  Quantity INT ,
- FOREIGN KEY (productId) REFERENCES productsTable(Id),
-  FOREIGN KEY (UserId) REFERENCES UserTable(Id))
- 
+FOREIGN KEY (productId) REFERENCES productsTable(Id),
+FOREIGN KEY (UserId) REFERENCES UserTable(Id))
+
+
+CREATE PROCEDURE insertOrder(@id VARCHAR(100), @userId VARCHAR(100) 
+,@productId VARCHAR(100) , @orderStatus VARCHAR(100),@quantity INT )
+AS
+BEGIN
+INSERT INTO Orders (Id, UserId, productId, orderStatus, Quantity)
+VALUES(@id,@userId,@productId,@orderStatus ,@quantity)
+END
+
+
+CREATE OR ALTER PROCEDURE  getUserOrders (@userid VARCHAR(100))
+AS
+BEGIN
+SELECT * FROM Orders WHERE UserId =@userid
+END
+
+
+CREATE PROCEDURE updateOrders(@id VARCHAR(100))
+AS
+
+BEGIN
+UPDATE Orders SET  orderStatus ='fulfilled' WHERE Id =@id
+END
+
+CREATE PROCEDURE deleteOrder(@orderId VARCHAR(100))
+AS
+
+BEGIN
+
+DELETE FROM Orders WHERE Id= @orderId
+
+END
