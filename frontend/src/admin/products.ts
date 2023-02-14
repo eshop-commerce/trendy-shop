be9591851c80d14848845c4f220db27c3fe79ce5
Milgo21 <felixmilgo21@gmail.com>
2023-02-15 00:08:07 +0300

    Fetching

--- frontend/src/admin/products.ts
+++ frontend/src/admin/products.ts
@@ -1,10 +1,20 @@
-let productDiv = document.getElementById("pro") as HTMLDivElement
+let productDiv = document.getElementById("items") as HTMLDivElement
+let addorupdate = document.getElementById("add-update-product") as HTMLButtonElement
+let addForm = document.querySelector('.add-new-product') as HTMLFormElement
+let addProduct = document.querySelector(".add-product") as HTMLButtonElement
+// input elements
+let product_name = document.getElementById("product-name") as HTMLInputElement
+let product_price = document.getElementById("product-pricee") as HTMLInputElement
+let product_image = document.getElementById("product-image") as HTMLInputElement
+let category = document.getElementById("product-category") as HTMLInputElement
+
+let BaseURL ="http://localhost:4004/products"
 interface Products{
     Id: string,
     Product_name:string,
     Product_price:string,
     Category_name: string
-    Product_image: string
+    Image_URL: string
   }
 let Product:Products[]=[]
 
@@ -24,8 +34,8 @@ let Product:Products[]=[]
 //         })
 // }
 
-function getProducts(){
-    fetch("http://localhost:4004/products")
+ function getProducts(){
+    fetch(BaseURL)
     .then((response) => response.json())
     .then((data :Products[]) => {
         Product = data
@@ -41,12 +51,57 @@ function showProduct(Product: Products[]) {
     console.log(Product);
     console.log('====================================');
     Product.forEach((a)=>{
-        let html =`
-        <div class="todo" onclick="">
-            <h1>${a.Product_name}</h1>
-            <p>${a.Product_price}</p>
-            <p>${a.Category_name}</p>
+        // let html =`
+        // <div class="todo" onclick="">
+        //     <h1>${a.Product_name}</h1>
+        //     <p>${a.Product_price}</p>
+        //     <p>${a.Category_name}</p>
+        // </div>
+        // `
+        let html=`
+        <div class="product__item" id="product">
+        <div class="product__item__pic ">
+            <img src="${a.Image_URL}" alt="item">
+            <div class="label new">New</div>
+            <ul class="product__hover">
+
+                <li>
+                    <a class="image-popup">
+                        <span class="arrow_expand">
+                            <ion-icon name="resize-outline" style="text-align: center;"></ion-icon>
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon_heart_alt">
+                            <ion-icon name="heart-outline" style="text-align: center;"></ion-icon>
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon_bag_alt">
+                            <ion-icon name="bag-outline" style="text-align: center;"></ion-icon>
+                        </span>
+                    </a>
+                </li>
+            </ul>
         </div>
+        <div class="product__item__text">
+            <h6>
+                <a href="#">${a.Product_name}</a>
+            </h6>
+            <div class="rating">
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+                <ion-icon name="star"></ion-icon>
+            </div>
+            <div class="product__price">$ ${a.Product_price}</div>
+        </div>
+    </div>
         `
     console.log(Product)
         productDiv.innerHTML += html
@@ -55,25 +110,50 @@ function showProduct(Product: Products[]) {
 // showProduct()
 
 
-
+addorupdate.addEventListener('click',updateProduct)
 function updateProduct(){
+    addForm.style.display="block"
+    const p_name = product_name.value
+    const p_image = product_image.value
+    const p_category = category.value
+    const p_price = product_price.value
+    if(p_name==''|| p_image=='' ||p_category=='' || p_price==''){
+        const p = document.createElement('p')
+        p.textContent='Please fill in all Fields'
+        p.style.color='red'
+        p.id='error-message'
+        addForm.insertAdjacentElement('afterbegin', p)
 
-}
+        setTimeout(()=>{
+            p.style.display='none'
+        },4000)}
+       else {
 
-function displayProducts(){
-    Product.forEach((a)=>{
-        let html =`
-        <div class="todo" onclick="">
-            <h1>${a.Product_name}</h1>
-            <p>${a.Product_price}</p>
-            <p>${a.Category_name}</p>
-        </div>
-        `
-    console.log(Product)
-        productDiv.innerHTML += html
-    })
+}}
+function postProduct(){
+    let payload = {
+        Product_name:"p_name",
+        Product_price:"p_price",
+        Category_name:"p_category",
+        Image_URL:"p_image"
+    }
+    let options ={
+        method:'POST',
+        body:JSON.stringify(payload)
+    }
+    fetch(BaseURL,options)
+    .then(response=>console.log(response.status));
 }
-displayProducts()
+// postProduct()
+// addProduct.addEventListener("click", postProduct)
+
+  
+
+    
+
+
+
+
 
 
 
@@ -85,10 +165,12 @@ displayProducts()
 //                           <p>${a.dates}</p>  
 //                           <p>${a.TaskName}</p>   
 //                           <p>${Math.abs(a.Days)} Streaks</p>  
-                                      
+
 //                   </div>`;
-  
+
 //         activities.innerHTML += html;
 //       });
 // }
+// update
+
 
